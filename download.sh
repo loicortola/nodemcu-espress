0dec410f36f461c7664617b6130fef73eab38871
Loïc Ortola <contact@loicortola.com>
2016-03-27 14:40:25 +0200

    Updated binaries

--- download.sh
+++ download.sh
@@ -1,22 +1,26 @@
 #/bin/bash
-nodemcu-tool download espress.lc --port $1
-nodemcu-tool download espress_init.lc --port $1
-nodemcu-tool download http_default_handler.lc --port $1
-nodemcu-tool download http_getondata.lc --port $1
-nodemcu-tool download http_prototypes.lc --port $1
-nodemcu-tool download http_request.lc --port $1
-nodemcu-tool download http_request_buffer.lc --port $1
-nodemcu-tool download http_request_processor.lc --port $1
-nodemcu-tool download http_response.lc --port $1
-nodemcu-tool download http_response_send.lc --port $1
-nodemcu-tool download http_response_sendfile.lc --port $1
-nodemcu-tool download auth_api_key.lc --port $1
-nodemcu-tool download router.lc --port $1
-nodemcu-tool download routes_auto.lc --port $1
-nodemcu-tool download http-200
-nodemcu-tool download http-400
-nodemcu-tool download http-401
-nodemcu-tool download http-403
-nodemcu-tool download http-404
-nodemcu-tool download http-405
-nodemcu-tool download http-503
+baud=$2
+if [ -z "$1" ]; then
+ echo "This script requires an argument: USB device port"
+ exit 1;
+fi
+if [ -z "$2" ]; then
+ echo "Setting baudrate to default: 115200"
+ baud=115200 
+ #Default nodemcu 1.5+ baudrate
+fi
+
+nodemcu-tool download espress.lc --port $1 --baud $baud
+nodemcu-tool download espress_init.lc --port $1 --baud $baud
+nodemcu-tool download http_default_handler.lc --port $1 --baud $baud
+nodemcu-tool download http_getondata.lc --port $1 --baud $baud
+nodemcu-tool download http_prototypes.lc --port $1 --baud $baud
+nodemcu-tool download http_request.lc --port $1 --baud $baud
+nodemcu-tool download http_request_buffer.lc --port $1 --baud $baud
+nodemcu-tool download http_request_processor.lc --port $1 --baud $baud
+nodemcu-tool download http_response.lc --port $1 --baud $baud
+nodemcu-tool download http_response_send.lc --port $1 --baud $baud
+nodemcu-tool download http_response_sendfile.lc --port $1 --baud $baud
+nodemcu-tool download auth_api_key.lc --port $1 --baud $baud
+nodemcu-tool download router.lc --port $1 --baud $baud
+nodemcu-tool download routes_auto.lc --port $1 --baud $baud
